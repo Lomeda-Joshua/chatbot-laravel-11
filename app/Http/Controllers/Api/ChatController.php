e124bae5c4d60f6d786babaddf0623355a26e5f3
JoshuaLomeda <joshualomeda.e14@gmail.com>
2026-04-14 11:18:02 +0800

    update: (for testing only) added static data and api to controller

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -11,14 +11,90 @@ class ChatController extends Controller
 {
     public function index()
     {
-        
         $data = ChatBotQueries::all();
         return response()->json($data);
     }
 
     public function data(Request $request)
     {
-        $data = ChatBotQueries::all();
-        return response()->json($data);
+        $data = collect([
+            (object)[
+                'id'      => 1,
+                'query'   => 'How may I help you today?',
+                'actions' => collect([                        
+                    (object)[
+                        'label'         => 'I would like to access the status of my complaint',
+                        'is_form'       => true,
+                        'is_submit'     => true,
+                        'next_sequence' => 0,
+                        'form'          => (object)[
+                            'description' => 'Please input your email address and tracking number',
+                            'fields'      => collect([        
+                                (object)[
+                                    'type'     => 'email',
+                                    'name'     => 'email',
+                                    'label'    => 'Email Address',
+                                    'value'    => '',
+                                    'required' => true,
+                                    'disabled' => false,
+                                    'options'  => collect([]) 
+                                ],
+                                (object)[
+                                    'type'     => 'text',
+                                    'name'     => 'tracking_number',
+                                    'label'    => 'Tracking Number',
+                                    'value'    => '',
+                                    'required' => true,
+                                    'disabled' => false,
+                                    'options'  => collect([]) 
+                                ],
+                            ]),
+                        ],
+                    ],
+                    (object)[
+                        'label'         => 'Speak to a live agent',
+                        'is_form'       => false,
+                        'is_submit'     => false,
+                        'next_sequence' => 2,
+                        'form'          => null,
+                    ],
+                ]),
+            ],
+        ]);
+
+        $formatted = $data->map(function ($query) {
+            return [
+                'id'      => $query->id,
+                'query'   => $query->query,
+                'actions' => $query->actions->map(function ($action) {
+                    return [
+                        'label'        => $action->label,
+                        'isForm'       => $action->is_form,
+                        'isSubmit'     => $action->is_submit,
+                        'nextSequence' => $action->next_sequence,
+                        'form'         => $action->form ? [
+                            'description' => $action->form->description,
+                            'fields'      => $action->form->fields->map(function ($field) {
+                                return [
+                                    'type'     => $field->type,
+                                    'name'     => $field->name,
+                                    'label'    => $field->label,
+                                    'value'    => $field->value ?? '',
+                                    'required' => $field->required,
+                                    'disabled' => $field->disabled,
+                                    'option'   => $field->options
+                                                    ->pluck('option_value')
+                                                    ->toArray(),
+                                ];
+                            })->toArray(),
+                        ] : null,
+                    ];
+                })->toArray(),
+            ];
+        });
+
+
+
+        return response()->json($formatted);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/', [ChatController::class, 'index'])->middleware('auth:sanctum');
+
+Route::get('/', [ChatController::class, 'data'])->middleware('auth:sanctum');
 
 
 Route::post('/token', function (Request $request) {
